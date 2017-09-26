e05cd05f36fd5562d88d95d07eaac691dda71180
Will Bruce <willtiger13@gmail.com>
2017-09-26 09:32:55 -0400

    Initial Working Commit

---
+++ src/main/java/com/will/betterforms/BetterformsApplication.java
@@ -0,0 +1,12 @@
+package com.will.betterforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterformsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterformsApplication.class, args);
+	}
+}
