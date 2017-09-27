cf47f9702234fcaf64cbdcbee1b07cf10a3944f5
Will Bruce <willtiger13@gmail.com>
2017-09-27 12:08:06 -0400

    added update and delete

--- src/main/java/com/will/betterforms/controllers/SecretController.java
+++ src/main/java/com/will/betterforms/controllers/SecretController.java
@@ -5,8 +5,10 @@ import com.will.betterforms.repositories.SecretRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SecretController {
@@ -21,5 +23,25 @@ public class SecretController {
         model.addAttribute("secret", mySecret);
         System.out.println(id);
         return "secret";
+    }
+
+    @RequestMapping(value = "/secret/{id}", method = RequestMethod.POST)
+    public String secretDetailForm(@ModelAttribute Secret secret) {
+        secretRepo.save(secret);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/secret/{id}/delete")
+    public String secreteDeleteConfirm(Model model,
+                                       @PathVariable("id") Long id) {
+        Secret secret = secretRepo.findOne(id);
+        model.addAttribute("secret", secret);
+        return "secretDeleteConfirm";
+    }
+
+    @RequestMapping(value = "/secret/{id}/delete", method = RequestMethod.POST)
+    public String secretDelete(@PathVariable("id") Long id) {
+        secretRepo.delete(id);
+        return "redirect:/";
     }
 }
