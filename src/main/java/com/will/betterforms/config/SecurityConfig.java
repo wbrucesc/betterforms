f15815e603e9af8cb8532d101d187f6b7265ab4f
Will Bruce <willtiger13@gmail.com>
2017-10-02 10:59:32 -0400

    Security config pretty much done.

--- src/main/java/com/will/betterforms/config/SecurityConfig.java
+++ src/main/java/com/will/betterforms/config/SecurityConfig.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -20,7 +23,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/webjars/**");
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/").hasRole("USER")
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .successHandler(successHandler())
+                    .failureHandler(failureHandler())
+                    .and()
+                .logout()
+                    .permitAll()
+                    .logoutSuccessUrl("/login");
+    }
+
+    private AuthenticationSuccessHandler successHandler() {
+        return (request, response, authentication) -> {
+            response.sendRedirect("/");
+        };
+    }
+
+    private AuthenticationFailureHandler failureHandler() {
+        return (request, response, exception) -> {
+            request.getSession().setAttribute("error", "Cannot login with provided credentials.");
+        };
+    }
+
 }
