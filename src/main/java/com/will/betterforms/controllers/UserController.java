08dc3e2b854db2e07cda198fd5da9c64aedfd00b
Will Bruce <willtiger13@gmail.com>
2017-10-03 09:53:38 -0400

    password encryption working. not yet comparing password hashes to hashed password in database

--- src/main/java/com/will/betterforms/controllers/UserController.java
+++ src/main/java/com/will/betterforms/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.will.betterforms.Models.User;
 import com.will.betterforms.repositories.RoleRepository;
 import com.will.betterforms.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,6 +21,10 @@ public class UserController {
     @Autowired
     private RoleRepository roleRepo;
 
+    //bring in bcrypt p/w encoder
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
     //displays signup page
     @RequestMapping(name = "/signup", method = RequestMethod.GET)
     public String signupForm(Model model) {
@@ -30,8 +35,12 @@ public class UserController {
     //receives post from signup form
     @RequestMapping(name = "/signup", method = RequestMethod.POST)
     public String signupForm(@ModelAttribute User user) {
-
+        //Role
         Role userRole = roleRepo.findByName("ROLE_USER");
+        //set the p/w to the encrypted version of what the p/w already is
+        String encryptedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        //overwrites plain text p/w prior to being sent to database
+        user.setPassword(encryptedPassword);
         user.setRole(userRole);
         userRepo.save(user);
         return "redirect:/login";
